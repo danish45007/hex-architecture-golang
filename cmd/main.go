5a1f0394ccc79e8f524bb55bbf0595ad1340ab5c
danish sharma <danish45007@gmail.com>
2021-09-13 02:37:57 +0530

    docker file and docker-compose added

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/danish45007/hex-architecture-golang/internal/adapters/app/api"
-
+	"github.com/danish45007/hex-architecture-golang/internal/adapters/core/arithmetic"
+	"github.com/danish45007/hex-architecture-golang/internal/adapters/framework/driven/db"
+	gRPC "github.com/danish45007/hex-architecture-golang/internal/adapters/framework/driving/grpc"
 	"github.com/danish45007/hex-architecture-golang/internal/ports"
+	"github.com/joho/godotenv"
 )
 
+func goDotEnvVariable(key string) string {
+	// load .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
 func main() {
+	var err error
 	//ports
 
 	//core(business-logic)
 	var core ports.ArithmeticPort
 	//application
-	var app ports.APIPort
-	//framework
-	var db ports.DBPort
-	app = api.NewAdapter(core, db)
-	res, err := app.GetAddition(1, 2)
+	var appAdapter ports.APIPort
+	//framework(database)
+	var dbAdapter ports.DBPort
+	//framework(gRPC)
+	var grpcAdapter ports.GrpcPort
+	dbDriver := goDotEnvVariable("DBDRIVER")
+	dbSource := goDotEnvVariable("DBSOURCENAME")
+
+	dbAdapter, err = db.NewAdapter(dbDriver, dbSource)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to initiate database %v", err)
 	}
-	fmt.Println(res)
+
+	defer dbAdapter.CloseDbConn()
+
+	core = arithmetic.NewAdapter()
+	appAdapter = api.NewAdapter(core, dbAdapter)
+	grpcAdapter = gRPC.NewAdapter(appAdapter)
+	grpcAdapter.Run()
 
 }
